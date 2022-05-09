3c7069c48cb1b76cb08de189bb540beb280431e9
Diego Real <diegoreal16@hotmail.com>
2022-05-09 13:29:10 +0200

    Diseño cutre login

--- login.php
+++ login.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <title>Login</title>
     <link href="css/css-principal.css" rel="stylesheet" type="text/css" media="screen" />
+    <link href="css/css-login.css" rel="stylesheet" type="text/css" media="screen" />
     <link href="css/css-mediaQueries.css" rel="stylesheet" type="text/css" media="screen" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="fotos/fotos-global/logo.png" sizes="64x64">
@@ -45,15 +46,30 @@
         header("location:index.php");
     } else {
     ?>
+        <div class="forma-login">
+            <div class="info">
+            <a href="index.php"><img src="fotos/fotos-global/logo1.png" width="140px" height="60px"/></a>
+            <h2>Real. Yatch. Rental.</h2>
+            <p>La nueva plataforma completamente pivada privada que te permite alquilar el barco perfecto para tus vacaciones, negocio, fiesta...</p>
+            </div>
+            <div>
+                <form action="login.php" method="post">
+                    <input type="text" name="usuario" class="nombre" placeholder="Usuario" /><br>
+                    <input type="password" name="clave" class="clave"placeholder="Contraseña" /><br>
+                    <input type="submit" class="button" value="Entrar" />
+                </form>
+            </div>
+        </div>
 
-        <form action="login.php" method="post">
-            <input type="text" name="usuario" placeholder="Usuario" />
-            <input type="password" name="clave" placeholder="contraseña" />
-            <input type="submit" value="validar" />
-        </form>
 
 
 
+        <div class="footer-login">
+            <p><a href="./aviso-legal.html">Aviso legal</a> |
+            <a href="./privacidad.html">Política de privacidad</a> |
+            <a href="./cookies.html">Política de cookies</a></p>
+            <p>RealYatchRental© 2022</p>
+        </div>
     <?php
     }
     ?>
